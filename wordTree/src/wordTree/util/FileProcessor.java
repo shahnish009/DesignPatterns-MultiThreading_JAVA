6a7edfbd31b13f5ee34be1cb182db926dcaeb030
Nishant Shah <nshah27@binghamton.edu>
2017-10-29 00:41:42 +0000

    FileProcessor created with method to read one line at a time

---
+++ wordTree/src/wordTree/util/FileProcessor.java
@@ -0,0 +1,50 @@
+package wordTree.util;
+
+import java.io.File;
+import java.util.Scanner;
+import java.io.FileNotFoundException;
+
+public class FileProcessor {
+	
+	private Scanner sc = null;
+
+	/**
+	 * constructor for FileProcessor
+	 * @param inputFile - file to be processed
+	 * @throws FileNotFoundException
+	 */
+	public FileProcessor(String inputFile) throws FileNotFoundException{
+		MyLogger.writeMessage("FileProcessor constructor called", MyLogger.DebugLevel.CONSTRUCTOR);
+		sc = new Scanner(new File(inputFile));
+	}
+
+	/**
+	 * method to read line from file line by line
+	 * @return one line at a time from the input file
+	 */
+	public String readLine() {
+		
+		try {
+			if(sc != null) {
+				while(sc.hasNext()) {
+					return sc.nextLine();
+				}
+			}
+			else {
+				throw new RuntimeException("unable to open file");
+			}
+			
+			if(sc != null) {
+				sc.close();
+				sc = null;
+			}
+			return null;
+		}
+		catch(Exception e) {
+			System.err.println(e + " occurred during readLine");
+			e.printStackTrace();
+			System.exit(1);
+		}
+		return null;
+	}
+}
