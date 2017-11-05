3555837df6aa8a7d6e8ee31ab14d5d24fa180509
Nishant Shah <nshah27@binghamton.edu>
2017-11-05 00:51:19 +0000

    driver updated with semantics of this assignment. starting to work with threads

--- wordTree/src/wordTree/driver/Driver.java
+++ wordTree/src/wordTree/driver/Driver.java
@@ -4,52 +4,49 @@ import java.lang.NumberFormatException;
 import java.lang.RuntimeException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import airportSecurityState.airportStates.SecurityFactors;
-import airportSecurityState.util.Results;
-import airportSecurityState.util.MyLogger;
-import airportSecurityState.util.FileProcessor;
+import wordTree.airportStates.SecurityFactors;
+import wordTree.store.Results;
+import wordTree.util.MyLogger;
+import wordTree.util.FileProcessor;
+import wordTree.util.InputProcessor;
 import java.util.InputMismatchException;
 import java.io.FileNotFoundException;
 
 public class Driver {
 	
-	private static enum prohibitedItems {
-		gun,
-		nailcutter,
-		blade,
-		knife
-	};
-
 	public static void main(String[] args) {
 
 		String ipFile = null;
 		String opFile = null;
-		String ip = null;
-		
-		double travellers = 0;
-		double days = 0;
-		double prohibited = 0;
+		String[] dlWords = null;
 		
 		int debugValueSetter = -1;
-		
-		String[] info = null;
-		String[] values = null;
 		
 		FileProcessor fileP = null;
 		
 		try {
-			if(3 == args.length) {
+			if(5 == args.length) {
        	        ipFile = args[0];
 	            opFile = args[1];
 	            
-	            debugValueSetter = Integer.parseInt(args[2]);
+	            final NUM_THREADS = Integer.parseInt(args[2]);
+	            if(!(NUM_THREADS >= 1 && NUM_THREADS <= 3)) {
+	            	throw new RuntimeException("Number of threads must be integers between 1 and 3 inclusive");
+	            }
+	            
+	            dlWords = args[3].split("\\s+");
+	            if(!(NUM_THREADS == dlWords.length)) {
+	            	throw new RuntimeException("Delete words should be same as number of threads");
+	            }
+	            
+	            debugValueSetter = Integer.parseInt(args[4]);
 	            if(!(debugValueSetter >= 0 && debugValueSetter <= 4)) {
 	            	throw new RuntimeException("Debug value must be integers between 0 and 4 inclusive");
 	            }
 	            MyLogger.setDebugValue(debugValueSetter);
 			}
 			else {
-				throw new RuntimeException("Invalid number of arguments. Please enter 3 arguments");
+				throw new RuntimeException("Invalid number of arguments. Please enter 5 arguments");
 			}
 		}
 		catch (Exception e) {
@@ -58,7 +55,7 @@ public class Driver {
 			System.exit(1);
 		}
 
-		try {
+/*		try {
 			fileP = new FileProcessor(ipFile);
 		}
 		catch(FileNotFoundException e) {
@@ -66,31 +63,15 @@ public class Driver {
 			e.printStackTrace();
 			System.exit(1);
 		}
-	
-		SecurityFactors secFac = new SecurityFactors(0,0);
-		Results r = new Results(opFile);
-		Driver d = new Driver();
-
-		while((ip = fileP.readLine()) != null) {
+*/	
+		//SecurityFactors secFac = new SecurityFactors(0,0);
+		//Results r = new Results(opFile);
+		//Driver d = new Driver();
+/*
+		while((ipWord = fileP.readLine()) != null) {
 
 			try {
-				info = ip.split(";");
-				values = info[0].split(":");
-				days = Double.parseDouble(values[1]);
-				values = null;
-
-				values = info[3].split(":");
-				
-				if(d.checkInEnum(values[1])) {
-					prohibited+=1;
-				}
-				
-				travellers+=1;
-				
-				secFac.setAvgTraffic(travellers / days);
-				secFac.setAvgProhibited(prohibited / days);
-				secFac.tightenOrLoosenSecurity();
-				
+								
 				r.storeNewResult(d.opRes(secFac));
 				
 				MyLogger.writeMessage(d.opRes(secFac) + " added to Results", MyLogger.DebugLevel.IN_RESULTS);
@@ -101,45 +82,14 @@ public class Driver {
 				System.exit(1);
 			}
 		}
-		
-		try {
+		*/
+/*		try {
 			r.writeToFile();
 		}
 		catch(FileNotFoundException e) {
 			System.err.println("Output file cannot be generated");
 			e.printStackTrace();
 			System.exit(1);
-		}
-	}
-	
-	private boolean checkInEnum(String s) {
-		
-		boolean retVal = false;
-		for(prohibitedItems pi : prohibitedItems.values()) {
-			if(pi.name().equalsIgnoreCase(s)) {
-				retVal = true;
-			}
-		}
-		return retVal;
-	}
-	
-	private String opRes(SecurityFactors sf) {
-		
-		ArrayList<Integer> al = new ArrayList<Integer>();
-		for(int i = 1; i <= 10; i++) {
-			al.add(i);
-		}
-		
-		if(0 == sf.getFlag()) {
-			return al.get(0) + " " + al.get(2) + " " + al.get(4) + " " + al.get(6) + " " + al.get(8); 
-		}
-		else if(1 == sf.getFlag()) {
-			return al.get(1) + " " + al.get(2) + " " + al.get(4) + " " + al.get(7) + " " + al.get(8); 
-		}
-		else if(2 == sf.getFlag()) {
-			return al.get(1) + " " + al.get(3) + " " + al.get(5) + " " + al.get(7) + " " + al.get(9); 
-		}
-		
-		return null;
+		}*/
 	}
 }
