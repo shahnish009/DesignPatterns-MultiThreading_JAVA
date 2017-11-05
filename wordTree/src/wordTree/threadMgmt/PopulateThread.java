2f0cbe1e92f65f3a266fd3df6609994c5f6fd163
Sean Annunciation <sannunc1@binghamton.edu>
2017-11-05 18:52:21 +0000

    PopulateThread structure created . yet to implement synchronized read

---
+++ wordTree/src/wordTree/threadMgmt/PopulateThread.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nishant_shah_sean_annunciation_assign_4.src.wordTree.threadMgmt;
+
+/**
+ *
+ * @author annse
+ */
+public class PopulateThread implements Runnable{
+    
+    
+        private final Object threadobject;
+        
+        public PopulateThread(Object throbj){
+            
+            threadobject=throbj;
+        }
+
+    public  void run(){
+        
+        
+        try{
+            //split the input
+            //insert into the tree
+            
+            
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+        
+		
+        
+		
+		
+	}
+	
+    
+}
